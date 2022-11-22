b13b18cd75d7546e9432174d7ef58444342ef2e7
AdrienBlass <adrien.blassel@gmail.com>
2022-11-22 12:10:27 +0100

    Creation des premières classes

--- src/main/java/bddweb/projet/entities/Carte.java
+++ src/main/java/bddweb/projet/entities/Carte.java
@@ -1,4 +1,9 @@
 package bddweb.projet.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Carte {
 }
--- src/main/java/bddweb/projet/entities/Client.java
+++ src/main/java/bddweb/projet/entities/Client.java
@@ -1,4 +1,5 @@
 package bddweb.projet.entities;
 
 public class Client {
+
 }
