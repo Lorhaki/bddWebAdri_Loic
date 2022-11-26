9dd11e55bc8f216637a3dbb29d78d5cb96af250d
AdrienBlass <adrien.blassel@gmail.com>
2022-11-26 11:07:55 +0100

    avancement sur PostCompte

--- src/main/java/bddweb/projet/services/CompteService.java
+++ src/main/java/bddweb/projet/services/CompteService.java
@@ -1,12 +1,19 @@
 package bddweb.projet.services;
 
+import bddweb.projet.controllers.communs.BadRequestException;
+import bddweb.projet.entities.Client;
 import bddweb.projet.entities.Compte;
 import bddweb.projet.repositories.ComptesRepository;
 //import bddweb.projet.services.dto.GetCompteResponse;
+import bddweb.projet.services.dto.CreateClientRequest;
+import bddweb.projet.services.dto.CreateClientResponse;
+import bddweb.projet.services.dto.comptes.CreateCompteRequest;
+import bddweb.projet.services.dto.comptes.CreateCompteResponse;
 import bddweb.projet.services.dto.comptes.GetCompteResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,6 +31,28 @@ public class CompteService {
                 .collect(Collectors.toList());
     }
 
+    public CreateCompteResponse createCompte(CreateCompteRequest compteToCreate)throws BadRequestException {
+        Compte compteToSave = this.comptesRepository.save(Compte.builder())
+                .intituleCompte(compteToCreate.getIntituleCompte())
+                .typeCompte(compteToCreate.getTypeCompte())
+                .titulaireCompte(compteToCreate.getTitulairesCompte())
+                .iban(compteToCreate.getIban())
+                .iban(compteToCreate.getIban())
+                .build());
+
+        return buildCreateCompteResponse(this.comptesRepository.save(compteToSave));
+    }
+    private CreateClientResponse buildCreateCompteResponse(Client client){
+        return CreateClientResponse.builder().
+                id(client.getId()).
+                nom(client.getNom()).
+                prenom(client.getPrenom()).
+                adressePostale(client.getAdressePostale()).
+                dateCreation(LocalDate.now()).
+                dateNaissance(client.getDateNaissance()).
+                telephone(client.getTelephonne()).
+                build();
+    }
     private GetCompteResponse buildGetComptesResponse(Compte compte) {
         return GetCompteResponse.builder().
                 iban(compte.getIban()).
