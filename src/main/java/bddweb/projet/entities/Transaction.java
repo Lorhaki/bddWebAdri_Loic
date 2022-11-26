4a474487a8baa6eb70dcc274e32b5621db00173d
Lorhaki <flament.lolo@laposte.net>
2022-11-26 10:04:14 +0100

    version qui fonctionne avec post et get client

--- src/main/java/bddweb/projet/entities/Transaction.java
+++ src/main/java/bddweb/projet/entities/Transaction.java
@@ -3,12 +3,17 @@ package bddweb.projet.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Entity
 public class Transaction{
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
 
 }
 
